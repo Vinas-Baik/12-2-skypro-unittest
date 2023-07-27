7a68263e93d5f3b148fda2be4e482ad71ef4b9ec
Victor Sadykov <svn1409@gmail.com>
2023-07-28 07:13:22 +0800

    Задание 5. Функция get_val
    Напишите минимум три теста для функции get_val, исходя из описания ее функционала.
    
    Функция возвращает значение из словаря по переданному ключу, если ключ существует.
    В ином случае возвращается значение default
    .

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -5,10 +5,10 @@ from utils import dicts
 class TestArrs(unittest.TestCase):
 
     def test_get_val(self):
-        self.assertEqual(arrs.get({"665932": "Baikalsk"}, "665932"), "Baikalsk")
-        self.assertEqual(arrs.get({"665932": "Baikalsk"}, "665932", "git_unitest"), "Baikalsk")
-        self.assertEqual(arrs.get({}, "665932", "git"), "git")
-        self.assertEqual(arrs.get({}, "665932"), "git")
-        self.assertEqual(arrs.get({}, "665932", "Irkutsk"), "Irkutsk")
+        self.assertEqual(dicts.get_val({"665932": "Baikalsk"}, "665932"), "Baikalsk")
+        self.assertEqual(dicts.get_val({"665932": "Baikalsk"}, "665932", "git_unitest"), "Baikalsk")
+        self.assertEqual(dicts.get_val({}, "665932", "git"), "git")
+        self.assertEqual(dicts.get_val({}, "665932"), "git")
+        self.assertEqual(dicts.get_val({}, "665932", "Irkutsk"), "Irkutsk")
 
 
--- utils/dicts.py
+++ utils/dicts.py
@@ -4,4 +4,7 @@ def get_val(collection, key, default='git'):
     Функция возвращает значение из словаря по переданному ключу, если ключ существует.
     В ином случае возвращается значение default
     """
-    pass
+    if collection == {}:
+        return default
+    else:
+        return collection[key]
